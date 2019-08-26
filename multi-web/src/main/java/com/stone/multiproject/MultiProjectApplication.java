03a90d10d4c3697df8ab348e93c06b63d8e3e0d0
TT <tt2stone@163.com>
2019-08-26 19:50:24 +0800

    init

---
+++ multi-web/src/main/java/com/stone/multiproject/MultiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stone.multiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiProjectApplication.class, args);
+    }
+
+}
